874191b98b51b2bca542323e56f635abe82be0cc
thanhpx04 <thanhpx04@gmail.com>
2022-10-21 12:41:09 +0700

    fetch data to see the data response

--- static/hello-world/src/App.js
+++ static/hello-world/src/App.js
@@ -3,6 +3,27 @@ import { invoke } from '@forge/bridge';
 import TableTree from '@atlaskit/table-tree';
 import staticData from './data/data.json';
 
+const projectName = 'TEST';
+const issueLinkName = `Parent of`;
+const data = async () => {
+    const params = `issueLinkType = ${issueLinkName} and project=${projectName}`;
+    const response = await requestJira(`/rest/api/2/search?jql=${params}`);
+    console.log('call api jira');
+    return await response.json();
+};
+
+const issueData = data().then((result) => {
+    let data = [];
+    console.log(result)
+    result.issues.forEach((element) => {
+        data.push({
+            label: element.key, value: element.key, visible: true,
+        });
+    });
+
+    return data;
+});
+
 /* eslint react/no-unused-prop-types: 0 */
 type ExampleItemData = { type: string, summary: string, status: string };
 
