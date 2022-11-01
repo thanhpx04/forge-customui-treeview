c0449c1dd7c6d9e336a2d65f46808334cbf02ea3
Thanh Pham <thanhpx04@gmail.com>
2022-11-01 09:30:12 +0700

    done dynamic column

--- static/hello-world/src/App.js
+++ static/hello-world/src/App.js
@@ -1,80 +1,114 @@
 import React, { useEffect, useState } from 'react';
 import { requestJira } from '@forge/bridge';
 import TableTree from '@atlaskit/table-tree';
-import DropdownMenu, {
-    DropdownItemCheckbox,
-    DropdownItemCheckboxGroup,
-  } from '@atlaskit/dropdown-menu';
+import DropdownMenu, { DropdownItemCheckbox, DropdownItemCheckboxGroup } from '@atlaskit/dropdown-menu';
 import Button from '@atlaskit/button';
 import staticData from './data/data.json';
 
-const projectName = 'TEST';
-const issueLinkName = `'Parent of'`;
-async function fetchData() {
-    const params = `issueLinkType = ${issueLinkName} and project=${projectName}`;
-    const response = await requestJira(`/rest/api/2/search?jql=${params}`);
-    console.log('===============================================================call api jira');
-    const data = await response.json();
-    console.log(data);
-}
+// const projectName = 'TEST';
+// const issueLinkName = 'Parent of';
+// async function fetchData() {
+//     const params = `issueLinkType = ${issueLinkName} and project=${projectName}`;
+//     const response = await requestJira(`/rest/api/2/search?jql=${params}`);
+//     console.log('===============================================================call api jira');
+//     const data = await response.json();
+//     console.log(data);
+// }
 
-/* eslint react/no-unused-prop-types: 0 */
-type ExampleItemData = { type: string, issuekey: string, summary: string, status: string };
-
-const Type = (props: ExampleItemData) => <span>{props.type}</span>;
-const IssueKey = (props: ExampleItemData) => <span>{props.issuekey}</span>;
-const Summary = (props: ExampleItemData) => <span>{props.summary}</span>;
-const Status = (props: ExampleItemData) => <span>{props.status}</span>;
+const IssueKey = (content) => <span>{content.issuekey}</span>;
+const Type = (content) => <span>{content.type}</span>;
+const Summary = (content) => <span>{content.summary}</span>;
+const Status = (content) => <span>{content.status}</span>;
 
 function App() {
-    let [listHeaders, setHeaders] = useState(['Type', 'Issue Key', 'Summary', 'Status']);
-    let [listColumns, setColumns] = useState([Type, IssueKey, Summary, Status]);
-    const [checked, setChecked] = useState<Record<string, boolean>>({
-        todo: true,
-      });
-      const toggle = (name: string) => {
+    let listMaster = [
+        {
+            header: 'Issue Key',
+            cell: IssueKey,
+            width: '200px',
+            isDisplay: true
+        },
+        {
+            header: 'Type',
+            cell: Type,
+            width: '200px',
+            isDisplay: true
+        },
+        {
+            header: 'Summary',
+            cell: Summary,
+            width: '400px',
+            isDisplay: true
+        },
+        {
+            header: 'Status',
+            cell: Status,
+            width: '200px',
+            isDisplay: true
+        }
+    ];
+
+    let [listHeaders, setHeaders] = useState(listMaster.map(item => item.header));
+    let [listColumnWidths, setColumnWidths] = useState(listMaster.map(item => item.width));
+    let [listColumns, setColumns] = useState([IssueKey, Type, Summary, Status]);
+    let [checked, setChecked] = useState({ issuekey: true, type: true, summary: true, status: true });
+
+    const toggle = (name) => {
+        switch (name) {
+            case 'issuekey':
+                listMaster[0].isDisplay = !checked['issuekey'];
+                listMaster[1].isDisplay = checked['type'];
+                listMaster[2].isDisplay = checked['summary'];
+                listMaster[3].isDisplay = checked['status'];
+                break;
+            case 'type':
+                listMaster[0].isDisplay = checked['issuekey'];
+                listMaster[1].isDisplay = !checked['type'];
+                listMaster[2].isDisplay = checked['summary'];
+                listMaster[3].isDisplay = checked['status'];
+                break;
+            case 'summary':
+                listMaster[0].isDisplay = checked['issuekey'];
+                listMaster[1].isDisplay = checked['type'];
+                listMaster[2].isDisplay = !checked['summary'];
+                listMaster[3].isDisplay = checked['status'];
+                break;
+            case 'status':
+                listMaster[0].isDisplay = checked['issuekey'];
+                listMaster[1].isDisplay = checked['type'];
+                listMaster[2].isDisplay = checked['summary'];
+                listMaster[3].isDisplay = !checked['status'];
+                break;
+            default:
+                break;
+        }
         setChecked((prev) => ({
-          ...prev,
-          [name]: !prev[name],
+            ...prev,
+            [name]: !prev[name],
         }));
-      };
-    // setHeaders()
+
+        setHeaders(listMaster.filter(item => item.isDisplay == true).map(item => item.header));
+        setColumnWidths(listMaster.filter(item => item.isDisplay == true).map(item => item.width));
+        setColumns(listMaster.filter(item => item.isDisplay == true).map(item => item.cell));
+    };
     return (
         <div>
-            <DropdownMenu trigger="Status">
-                <DropdownItemCheckboxGroup title="Categories" id="actions">
-                    <DropdownItemCheckbox
-                    id="todo"
-                    onClick={(e) => toggle('todo')}
-                    isSelected={checked['todo']}
-                    >
-                    To do
-                    </DropdownItemCheckbox>
-                    <DropdownItemCheckbox
-                    id="inprogress"
-                    onClick={(e) => toggle('inprogress')}
-                    isSelected={checked['inprogress']}
-                    >
-                    In progress
-                    </DropdownItemCheckbox>
-                    <DropdownItemCheckbox
-                    id="done"
-                    onClick={(e) => toggle('done')}
-                    isSelected={checked['done']}
-                    >
-                    Done
-                    </DropdownItemCheckbox>
-                </DropdownItemCheckboxGroup>
-            </DropdownMenu>
-            <p></p>
             <TableTree
                 headers={listHeaders}
+                columnWidths={listColumnWidths}
                 columns={listColumns}
-                columnWidths={['200px', '200px', '400px', '100px']}
                 items={staticData.children}
                 />
-            <br></br>
-            <Button appearance="primary" onclick="fetchData()">Fetch Data</Button>
+            <p></p>
+            <DropdownMenu trigger="Select display columns">
+                <DropdownItemCheckboxGroup title="Column" id="actions">
+                    <DropdownItemCheckbox id="issuekey" onClick={(e) => toggle('issuekey')} isSelected={checked['issuekey']}>Issue Key</DropdownItemCheckbox>
+                    <DropdownItemCheckbox id="type" onClick={(e) => toggle('type')} isSelected={checked['type']}>Type</DropdownItemCheckbox>
+                    <DropdownItemCheckbox id="summary" onClick={(e) => toggle('summary')} isSelected={checked['summary']}>Summary</DropdownItemCheckbox>
+                    <DropdownItemCheckbox id="status" onClick={(e) => toggle('status')} isSelected={checked['status']}>Status</DropdownItemCheckbox>
+                </DropdownItemCheckboxGroup>
+            </DropdownMenu>
+            {/* <Button appearance="primary" onClick="fetchData()">Fetch Data</Button> */}
         </div>
     );
 }
