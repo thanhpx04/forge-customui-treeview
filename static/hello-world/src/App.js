639b3b9b00ea9a5c3e4fbdbcc80f7f5e99681321
Thanh Pham <thanhpx04@gmail.com>
2022-11-11 17:39:27 +0700

    done add new child with static data

--- static/hello-world/src/App.js
+++ static/hello-world/src/App.js
@@ -1,67 +1,56 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { requestJira } from '@forge/bridge';
-import TableTree from '@atlaskit/table-tree';
+import TableTree, { Cell, Header, Headers, Row, Rows, TableTreeDataHelper, } from '@atlaskit/table-tree';
 import DropdownMenu, { DropdownItemCheckbox, DropdownItemCheckboxGroup } from '@atlaskit/dropdown-menu';
 import Button from '@atlaskit/button';
+// import Textfield from '@atlaskit/textfield';
 import AddIcon from '@atlaskit/icon/glyph/add'
 import EditIcon from '@atlaskit/icon/glyph/edit'
-import staticData from './data/data.json';
+import staticData from './data/dataArrayType.json';
+import sampleData from './data/sampleData.json';
 
-const IssueKey = (content) => <span>{content.issuekey}</span>;
-const Type = (content) => <span>{content.type}</span>;
-const Summary = (content) => <span>{content.summary}</span>;
-const Status = (content) => <span>{content.status}</span>;
-const Actions = (content) => {
-    const addHandler = () => {
-        console.log(content);
-    }
-    
-    const editHandler = () => {
-        console.log("edit");
-    }
-    return (
-        <div>
-            <Button iconBefore={<AddIcon label="" />} appearance="subtle" onClick={addHandler}></Button>
-            <Button iconBefore={<EditIcon label="" />} appearance="subtle" onClick={editHandler}></Button>
-        </div>
-    );
-}
+const tableTreeHelper = new TableTreeDataHelper({ key: 'issuekey' });
+
+const loadTreeData = () => {
+    return tableTreeHelper.updateItems(staticData);
+};
+
+const fetchNewItems = async () => {
+    return [sampleData,];
+};
 
 function App() {
     const [dataMaster, setChecked] = useState({
         issuekey: {
             header: 'Issue Key',
-            cell: IssueKey,
-            width: '200px',
+            width: '250px',
             isDisplay: true
         },
         type: {
             header: 'Type',
-            cell: Type,
             width: '200px',
             isDisplay: true
         },
         summary: {
             header: 'Summary',
-            cell: Summary,
             width: '400px',
             isDisplay: true
         },
         status: {
             header: 'Status',
-            cell: Status,
             width: '200px',
             isDisplay: true
         },
         actions: {
             header: 'Actions',
-            cell: Actions,
             width: '200px',
             isDisplay: true
         }
     });
+    const [itemsRow, setItemsRow] = useState(loadTreeData);
+    const [currentAdding, setCurrentAdding] = useState('0');
 
-    function getDisplayItems(dataMaster) {
+    const getDisplayItems = (dataMaster) => {
         let result = [];
         for (let key in dataMaster) {
             if (dataMaster[key].isDisplay) {
@@ -78,14 +67,62 @@ function App() {
         }));
 
     };
+
+    const addRowHandler = useCallback((currentItem) => {
+        console.log('currentItem');
+        console.log(currentItem);
+        fetchNewItems().then((newItems) => {
+            console.log('newItems');
+            console.log(newItems);
+            setItemsRow((items) =>
+                tableTreeHelper.appendItems(newItems, items, currentItem),
+            );
+        });
+    }, []);
+    // = (item) => {
+    //     console.log("add");
+    //     console.log(item);
+    //     // let index = itemsRow.findIndex(item => item.issuekey === issuekey);
+    //     // console.log(index);
+    //     setItems((itemsRow) =>
+    //         tableTreeHelper.appendItems(sampleData, itemsRow, item)
+    //     );
+    //     // setCurrentAdding(issuekey);
+    // }
+
+    const editRowHandler = (issuekey) => {
+        console.log("edit");
+        console.log(issuekey);
+    }
     return (
         <div>
-            <TableTree
-                headers={getDisplayItems(dataMaster).map(i => i.header)}
-                columnWidths={getDisplayItems(dataMaster).map(i => i.width)}
-                columns={getDisplayItems(dataMaster).map(i => i.cell)}
-                items={staticData.children}
-            />
+            <TableTree>
+                <Headers>
+                    {getDisplayItems(dataMaster).map((item) => {
+                        return (
+                            <Header width={item.width}>{item.header}</Header>
+                        );
+                    })}
+                </Headers>
+                <Rows
+                    items={itemsRow}
+                    render={({ issuekey, type, summary, status, children = [] }) =>
+                        issuekey === currentAdding ? (
+                            <Button appearance="primary">Retry</Button>
+                        ) : (
+                            <Row itemId={issuekey} items={children} hasChildren={children.length > 0}>
+                                <Cell>{issuekey}</Cell>
+                                <Cell>{type}</Cell>
+                                <Cell>{summary}</Cell>
+                                <Cell>{status}</Cell>
+                                <Cell>
+                                    <Button iconBefore={<AddIcon label="" />} appearance="subtle" onClick={(e) => addRowHandler({ issuekey, type, summary, status, children })}></Button>
+                                    <Button iconBefore={<EditIcon label="" />} appearance="subtle" onClick={(e) => editRowHandler(issuekey)}></Button>
+                                </Cell>
+                            </Row>
+                        )}
+                />
+            </TableTree>
             <p></p>
             <DropdownMenu trigger="Select display columns">
                 <DropdownItemCheckboxGroup title="Column" id="actions">
